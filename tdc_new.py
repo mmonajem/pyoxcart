bbc210c77153e9420a1194e64712a8730582e28b
dasistharsh <harsh.pandey@fau.de>
2022-01-04 22:56:11 +0100

    added documentation for tdc_new.py

--- tdc_new.py
+++ tdc_new.py
@@ -15,6 +15,9 @@ QUEUE_ENDOFMEAS = 1
 
 
 class BufDataCB4(scTDC.buffered_data_callbacks_pipe):
+    '''
+    The class inherits from python wrapper module scTDC and class: buffered_data_callbacks_pipe
+    '''
     def __init__(self, lib, dev_desc, raw_mode, dld_events,
                  max_buffered_data_len=500000):
         if not raw_mode:
@@ -29,16 +32,47 @@ class BufDataCB4(scTDC.buffered_data_callbacks_pipe):
                 | scTDC.SC_DATA_FIELD_CHANNEL \
                 | scTDC.SC_DATA_FIELD_START_COUNTER
 
+        
+        '''
+        Initialize the base class: scTDC.buffered_data_callbacks_pipe
+
+        Atrributes
+            lib : scTDClib- a scTDClib object.
+            dev_desc : int
+                device descriptor as returned by sc_tdc_init_inifile(...).
+            data_field_selection : int, optional
+                a 'bitwise or' combination of SC_DATA_FIELD_xyz constants. The
+                default is SC_DATA_FIELD_TIME.
+            max_buffered_data_len : int, optional
+                The number of events that are buffered before invoking the on_data
+                callback. Less events can also be received in the on_data callback,
+                when the user chooses to return True from the on_end_of_meas
+                callback.
+                The default is (1<<16).
+            dld_events : bool, optional
+                if True, receive DLD events. If False, receive TDC events.
+                Depending on the configuration in the tdc_gpx3.ini file, only one
+                type of events may be available. The default is True.
+        '''
         super().__init__(lib, dev_desc, data_field_selection,  # <-- mandatory!
                          max_buffered_data_len, dld_events)    # <-- mandatory!
 
+        # Initialize the queue object
         self.queue = Queue()
+        # Setting the flag
         self.end_of_meas = False
 
     def on_data(self, d):
-        # make a dict that contains copies of numpy arrays in d ("deep copy")
-        # start with an empty dict, insert basic values by simple assignment,
-        # insert numpy arrays using the copy method of the source array
+        '''
+        This class method fucntion
+            1. Makes a dict that contains copies of numpy arrays in d ("deep copy")
+            2. Start with an empty dict, insert basic values by simple assignment,
+            3. Insert numpy arrays using the copy method of the source array
+        Attributes:
+            d: [dictionary]
+        Returns:
+            Does not return anything
+        '''
         dcopy = {}
         for k in d.keys():
             if isinstance(d[k], np.ndarray):
@@ -51,6 +85,15 @@ class BufDataCB4(scTDC.buffered_data_callbacks_pipe):
             self.queue.put((QUEUE_ENDOFMEAS, None))
 
     def on_end_of_meas(self):
+        '''
+        This class method set end_of_meas to True
+
+        Atrributes:
+            Does not accept any argument
+        Retuns
+            True [boolean]
+        '''
+
         self.end_of_meas = True
         # setting end_of_meas, we remember that the next on_data delivers the
         # remaining data of this measurement
@@ -67,19 +110,23 @@ def experiment_measure(raw_mode, queue_x,
                        queue_tdc_start_counter,
                        queue_stop_measurement):
     """
-    measurement function
-    Parameters
-    ----------
-    DLD Queues: queue
-        Queues that contains DLD data
-    TDC Queues: queue
-        Queues that contains TDC raw data
-    Stop measurement flag: queue
-        Queue for stop the measurement. This queue is set to True from oxcart.py
+    measurement function: This function is called in a process by 
+                          oxcart.py tp read data from the queue.
+    Atrributes:
+        DLD Queues: Queues that contains DLD data
+            queue_y: Queue for grp: DLD and parameter: y
+            queue_t: Queue for grp: DLD and parameter: t
+            queue_dld_start_counter: Queue for grp: DLD and parameter: start_counter
+            
+        TDC Queues: Queues that contains TDC raw data
+            queue_time_data: Queue for grp: TDC and parameter: time_data
+            queue_tdc_start_counter: Queue for grp: TDC and parameter: start_counter
+
+        Stop measurement flag: queue
+            Queue for stop the measurement. This queue is set to True from oxcart.py
 
     Returns
-    -------
-    None
+        Does not return anything
     """
 
     device = scTDC.Device(autoinit=False)
@@ -97,8 +144,18 @@ def experiment_measure(raw_mode, queue_x,
     # open a BUFFERED_DATA_CALLBACKS pipe
     bufdatacb = BufDataCB4(device.lib, device.dev_desc, raw_mode, dld_events=not raw_mode)
 
-    # define a closure that checks return codes for errors and does clean up
     def errorcheck(retcode):
+        '''
+        This fucntion define a closure that checks return codes for errors and does clean up.
+        
+        Attributes:
+            retcode: Return code
+        
+        Returns:
+            0: if success return code or return code > 0 [int]
+            -1: if return code is error code or less than 0 [int]
+
+        '''
         if retcode < 0:
             print(device.lib.sc_get_err_msg(retcode))
             bufdatacb.close()
@@ -113,8 +170,8 @@ def experiment_measure(raw_mode, queue_x,
     if errorcheck(retcode) < 0:
         return -1
 
+    # TODO: No time.sleep() in infinite loop: could utilze execessive CPU
     while True:
-        # start = time.time()
         eventtype, data = bufdatacb.queue.get()  # waits until element available
         if eventtype == QUEUE_DATA:
             if not raw_mode:
@@ -140,7 +197,8 @@ def experiment_measure(raw_mode, queue_x,
 
     time.sleep(0.1)
     # clean up
-    bufdatacb.close() # closes the user callbacks pipe, method inherited from base class
+    # closes the user callbacks pipe, method inherited from base class
+    bufdatacb.close() 
     device.deinitialize()
 
     return 0
