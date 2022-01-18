9fb1d9424806c5811559c1d7e936cd2c01c32e2c
dasistharsh <harsh.pandey@fau.de>
2022-01-18 14:23:47 +0100

    added documentation for tweet_send.py

--- devices/tweet_send.py
+++ devices/tweet_send.py
@@ -7,6 +7,15 @@ access_token_secret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 
 def tweet_send(message):
+    '''
+    This functions update status on twitter.
+
+    Attributes:
+        message: message to be updated as status in twitter [string]
+    Returns:
+        Does not return anything.
+
+    '''
     # Authenticate to Twitter
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
