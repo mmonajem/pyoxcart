a39db3eaba8a373b7fb3d16c7dae8db3117f0548
Mehrpad Monajem <mehrpad.monajem@gmail.com>
2022-09-26 15:28:18 +0200

    reconstruction.py update

--- pyccapt/calibration/calibration_tools/reconstruction.py
+++ pyccapt/calibration/calibration_tools/reconstruction.py
@@ -24,7 +24,7 @@ def pol2cart(rho, phi):
     return x, y
 
 
-def atom_probe_recons_from_detector(detx, dety, hv, flight_path_length, kf, det_eff, icf, field_evap, avg_dens):
+def atom_probe_recons_from_detector_Gault_et_al(detx, dety, hv, flight_path_length, kf, det_eff, icf, field_evap, avg_dens):
     """
     # atom probe reconstruction after: Gault et al., Ultramicroscopy 111(2011) 448 - 457
     x, y are the detector hit coordinates in mm
@@ -57,7 +57,10 @@ def atom_probe_recons_from_detector(detx, dety, hv, flight_path_length, kf, det_
 
     # launch angle relative to specimen axis
     theta_p = np.arctan(rad / (flight_path_length * 1E-3))  # mm / mm
+
     thet_a = theta_p + np.arcsin((kf - 1) * np.sin(theta_p))
+
+    icf_2 = thet_a / theta_p
 
     # distance from axis and z shift of each hit
     z_p, d = pol2cart(radius_evolution, thet_a)  # nm
@@ -74,8 +77,10 @@ def atom_probe_recons_from_detector(detx, dety, hv, flight_path_length, kf, det_
     omega = 1E-9 ** 3 / avg_dens  # atomic volume in nm ^ 3
 
     # nm ^ 3 * mm ^ 2 * V ^ 2 / nm ^ 2 / (mm ^ 2 * V ^ 2)
-    dz = omega * ((flight_path_length * 1E-3) ** 2) * (kf ** 2) / (det_eff * det_area * (icf ** 2)) * (
-                hv ** 2)
+    #     dz = omega * ((flight_path_length * 1E-3) ** 2) * (kf ** 2)  / (det_eff * det_area * (icf ** 2)) * (
+    #                 hv ** 2)
+    dz = (omega * ((flight_path_length * 1E-3) ** 2) * (kf ** 2) * ((field_evap / 1E-9) ** 2)) / (
+                det_area * det_eff * (icf_2 ** 2) * (hv ** 2))
     # wide angle correction
     cum_z = np.cumsum(dz)
     z = cum_z + z_p
@@ -83,29 +88,43 @@ def atom_probe_recons_from_detector(detx, dety, hv, flight_path_length, kf, det_
     return x * 1E9, y * 1E9, z * 1E9
 
 
-def atom_probe_recons_pad(detx, dety, hv, flight_path_length, ion_volume, f_evap, det_eff, kf, icf):
+def atom_probe_recons_bas_et_al(detx, dety, hv, flight_path_length, kf, det_eff, icf, field_evap, avg_dens):
     """
     :param detx: Hit position on the detector
     :param dety: Hit position on the detector
     :param hv: High voltage
     :param kf: Field reduction factor
-    :param icf: Image compression factor (1.4) Because sample is not a perfect sphere
+    :param icf: Image compression factor Because sample is not a perfect sphere
     :param flight_path_length: distance between detector and sample
     :param ion_volume: atomic volume in atoms/nm ^ 3
     :param det_eff: Efficiency of the detector
     :return:
     """
     # f_evap   evaporation field in V / nm
-    radius_evolution = hv / (icf * (f_evap / 1E-9))
+    radius_evolution = hv / (icf * (field_evap / 1E-9))
 
     m = (flight_path_length * 1E-3) / (kf * radius_evolution)
 
     x = (detx * 1E-3) / m
     y = (dety * 1E-3) / m
 
-    det_area = ((78 * 1E-3)/2)**2 * np.pi
+    # detector coordinates in polar form
+    rad, ang = cart2pol(detx * 1E-3, dety * 1E-3)
+    # calculating effective detector area:
+    det_area = (np.max(rad) ** 2) * np.pi
 
-    dz = (((1/ion_volume) * 1E-9 ** 3) * (110 * 1E-3) ** 2) / (det_area * det_eff * (icf ** 2) * (radius_evolution ** 2))
-    z = np.cumsum(dz)
+    # accumulative part of z
+    omega = 1E-9 ** 3 / avg_dens  # atomic volume in nm ^ 3
 
-    return x*1E9, y*1E9, z*1E9
+    #     dz = ((omega * (110 * 1E-3)**2) / (det_area * det_eff * (icf ** 2) * (radius_evolution ** 2))
+
+    dz = (omega * ((flight_path_length * 1E-3) ** 2) * (kf ** 2) * ((field_evap / 1E-9) ** 2)) / (
+                det_area * det_eff * (icf ** 2) * (hv ** 2))
+
+    #     dz_p = radius_evolution * (1 - np.sqrt((x**2 + y**2) / (radius_evolution**2)))
+    dz_p = radius_evolution * (1 - np.sqrt(1 - ((x ** 2 + y ** 2) / (radius_evolution ** 2))))
+    #     dz_p = radius_evolution - np.sqrt((x**2) + (y**2))
+
+    z = np.cumsum(dz) + dz_p
+
+    return x * 1E9, y * 1E9, z * 1E9
