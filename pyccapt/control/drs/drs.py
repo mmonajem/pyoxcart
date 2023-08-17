ce3c7d69ca8decd93c3e57d0183384f91b9b1627
Mehrpad Monajem <mehrpad.monajem@gmail.com>
2023-08-17 15:19:06 +0200

    fixed drs error

--- pyccapt/control/drs/drs.py
+++ pyccapt/control/drs/drs.py
@@ -4,16 +4,13 @@ import os
 import numpy as np
 from numpy.ctypeslib import ndpointer
 
-# local imports
-from pyccapt.control.control_tools import loggi
-
 
 class DRS:
     """
     This class sets up the parameters for the DRS group and allows users to read experiment DRS values.
     """
 
-    def __init__(self, trigger, test, delay, sample_frequency, log, log_path):
+    def __init__(self, trigger, test, delay, sample_frequency):
         """
         Constructor function which initializes function parameters.
 
@@ -41,10 +38,6 @@ class DRS:
         self.drs_lib.Drs_delete_drs_ox.restype = ctypes.c_void_p
         self.drs_lib.Drs_delete_drs_ox.argtypes = [ctypes.c_void_p]
         self.obj = self.drs_lib.Drs_new(trigger, test, delay, sample_frequency)
-        self.log = log
-        self.log_path = log_path
-        if self.log:
-            self.log_drs = loggi.logger_creator('drs', 'dsr.log', path=self.log_path)
 
     def reader(self):
         """
@@ -64,52 +57,53 @@ class DRS:
         """
         self.drs_lib.Drs_delete_drs_ox(self.obj)
 
+
 def experiment_measure(variables):
-	"""
-	Continuously reads the DRS data and puts it into the queues.
+    """
+    Continuously reads the DRS data and puts it into the queues.
 
-	Args:
-		variables: Variables object
-	"""
-	drs_ox = DRS(trigger=0, test=1, delay=0, sample_frequency=2, log=log, log_path=log_path)
+    Args:
+        variables: Variables object
+    """
+    drs_ox = DRS(trigger=0, test=1, delay=0, sample_frequency=2)
 
-	while True:
-		returnVale = np.array(drs_ox.reader())
-		data = returnVale.reshape(8, 1024)
-		# with self.variables.lock_data:
-		ch0_time = data[0, :]
-		ch0_wave = data[1, :]
-		ch1_time = data[2, :]
-		ch1_wave = data[3, :]
-		ch2_time = data[4, :]
-		ch2_wave = data[5, :]
-		ch3_time = data[6, :]
-		ch3_wave = data[7, :]
+    while True:
+        returnVale = np.array(drs_ox.reader())
+        data = returnVale.reshape(8, 1024)
+        # with self.variables.lock_data:
+        ch0_time = data[0, :]
+        ch0_wave = data[1, :]
+        ch1_time = data[2, :]
+        ch1_wave = data[3, :]
+        ch2_time = data[4, :]
+        ch2_wave = data[5, :]
+        ch3_time = data[6, :]
+        ch3_wave = data[7, :]
 
-		variables.extend_to('ch0_time', ch0_time.tolist())
-		variables.extend_to('ch0_wave', ch0_wave.tolist())
-		variables.extend_to('ch1_time', ch1_time.tolist())
-		variables.extend_to('ch1_wave', ch1_wave.tolist())
-		variables.extend_to('ch2_time', ch2_time.tolist())
-		variables.extend_to('ch2_wave', ch2_wave.tolist())
-		variables.extend_to('ch3_time', ch3_time.tolist())
-		variables.extend_to('ch3_wave', ch3_wave.tolist())
+        variables.extend_to('ch0_time', ch0_time.tolist())
+        variables.extend_to('ch0_wave', ch0_wave.tolist())
+        variables.extend_to('ch1_time', ch1_time.tolist())
+        variables.extend_to('ch1_wave', ch1_wave.tolist())
+        variables.extend_to('ch2_time', ch2_time.tolist())
+        variables.extend_to('ch2_wave', ch2_wave.tolist())
+        variables.extend_to('ch3_time', ch3_time.tolist())
+        variables.extend_to('ch3_wave', ch3_wave.tolist())
 
-		voltage_data = np.tile(variables.specimen_voltage, len(ch0_time))
-		pulse_data = np.tile(variables.pulse_voltage, len(ch0_time))
-		variables.extend_to('main_v_dc_drs', voltage_data.tolist())
-		variables.extend_to('main_p_drs', pulse_data.tolist())
+        voltage_data = np.tile(variables.specimen_voltage, len(ch0_time))
+        pulse_data = np.tile(variables.pulse_voltage, len(ch0_time))
+        variables.extend_to('main_v_dc_drs', voltage_data.tolist())
+        variables.extend_to('main_p_drs', pulse_data.tolist())
 
-		# with self.variables.lock_data_plot:
-		variables.extend_to('main_v_dc_plot', voltage_data.tolist())
-		# we have to calculate x and y from the wave data here
-		variables.extend_to('x_plot', ch0_time.tolist())
-		variables.extend_to('y_plot', ch0_time.tolist())
-		variables.extend_to('t_plot', ch0_time.tolist())
+        # with self.variables.lock_data_plot:
+        variables.extend_to('main_v_dc_plot', voltage_data.tolist())
+        # we have to calculate x and y from the wave data here
+        variables.extend_to('x_plot', ch0_time.tolist())
+        variables.extend_to('y_plot', ch0_time.tolist())
+        variables.extend_to('t_plot', ch0_time.tolist())
 
-		if variables.flag_stop_tdc:
-			print('DRS loop is break in child process')
-			break
+        if variables.flag_stop_tdc:
+            print('DRS loop is break in child process')
+            break
 
     drs_ox.delete_drs_ox()
 
