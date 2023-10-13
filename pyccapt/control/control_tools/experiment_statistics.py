6cf1f6c183a203d24d2499bf5a4e573411b5ffbd
Mehrpad Monajem <mehrpad.monajem@gmail.com>
2023-10-13 16:37:09 +0200

    statistic file

--- pyccapt/control/control_tools/experiment_statistics.py
+++ pyccapt/control/control_tools/experiment_statistics.py
@@ -1,40 +1,65 @@
+import datetime
+
+
 def save_statistics_apt(variables):
-	"""
-	Save setup parameters and run statistics in a text file.
+    """
+    Save setup parameters and run statistics in a text file.
 
-	Args:
-		variables (object): An object containing experiment variables.
+    Args:
+        variables (object): An object containing experiment variables.
 
-	Returns:
-		None
-	"""
-	# Save setup parameters and run statistics in a text file
-	with open(variables.path + '\\parameters.txt', 'w') as f:
-		f.write('Username: ' + variables.user_name + '\n')
-		f.write('Experiment Name: ' + variables.ex_name + '\n')
-		f.write('Maximum Experiment Time: ' + variables.ex_time + '\n')
-		f.write('Maximum Number of Ions: %s\n' % variables.max_ions)
-		f.write('Control Refresh Frequency: %s\n' % variables.ex_freq)
-		f.write('Specimen DC Min Voltage (V): %s\n' % variables.vdc_min)
-		f.write('Specimen DC Max Voltage (V): %s\n' % variables.vdc_max)
-		f.write('K_p Upwards: %s\n' % variables.vdc_step_up)
-		f.write('K_p Downwards: %s\n' % variables.vdc_step_down)
-		f.write('Control Algorithm: %s\n' % variables.control_algorithm)
-		f.write('Pulse Mode: %s\n' % variables.pulse_mode)
-		f.write('Pulse Min Voltage (V): %s\n' % variables.v_p_min)
-		f.write('Pulse Max Voltage (V): %s\n' % variables.v_p_max)
-		f.write('Pulse Fraction (%s): %s\n' % ('%', variables.pulse_fraction))
-		f.write('Pulse Frequency (Khz): %s\n' % variables.pulse_frequency)
-		f.write('Detection Rate (%s): %s\n' % ('%', variables.detection_rate))
-		f.write('Counter source: %s\n' % variables.counter_source)
-		f.write('Email: ' + variables.email + '\n')
+    Returns:
+        None
+    """
+    # Get the current date and time
+    current_datetime = datetime.datetime.now()
 
-		if variables.pulse_mode == 'Voltage':
-			f.write('Pulse start Voltage (V): %s\n' % variables.v_p_min)
-			f.write('Pulse Stop Voltage (V): %s\n' % variables.v_p_max)
+    # Create a header with additional information
+    header = f"""
+    Experiment Parameters and Statistics
+    -------------------------------------------
+    Experiment Timestamp: {current_datetime}
+    User Information:
+        Username: {variables.user_name}
+        Email: {variables.email}
 
-		f.write('Experiment Elapsed Time (Sec): %s\n' % "{:.3f}".format(variables.elapsed_time))
-		f.write('Experiment Total Ions: %s\n' % variables.total_ions)
-		f.write('Specimen Max Achieved Voltage (V): %s\n' % "{:.3f}".format(variables.specimen_voltage))
-		f.write('Specimen Max Achieved Pulse Voltage (V): %s\n' % "{:.3f}".format(variables.pulse_voltage))
-		f.write('Last detection rate: %s\n' % ('%', "{:.3f}".format(variables.detection_rate_current_plot)))
+    Experiment Setup Parameters:
+        Experiment Name: {variables.ex_name}
+        Maximum Experiment Time: {variables.ex_time} seconds
+        Maximum Number of Ions: {variables.max_ions}
+        Control Refresh Frequency: {variables.ex_freq} Hz
+        Specimen DC Voltage Range (Min-Max): {variables.vdc_min} V - {variables.vdc_max} V
+        K_p Upwards: {variables.vdc_step_up}
+        K_p Downwards: {variables.vdc_step_down}
+        Control Algorithm: {variables.control_algorithm}
+        Pulse Mode: {variables.pulse_mode}
+    """
+
+    if variables.pulse_mode == 'Voltage':
+        header += f"""
+        Pulse Voltage Range (Min-Max): {variables.v_p_min} V - {variables.v_p_max} V
+        """
+
+    header += f"""
+    Pulse Fraction: {variables.pulse_fraction}%
+    Pulse Frequency: {variables.pulse_frequency} kHz
+    Detection Rate: {variables.detection_rate}%
+    Counter Source: {variables.counter_source}
+    -----------------------------------------------------
+    """
+
+    # Save setup parameters and run statistics in a text file
+    statistics = f"""
+    Experiment Elapsed Time (Sec): {variables.elapsed_time:.3f}
+    Experiment Total Ions: {variables.total_ions}
+    Specimen Max Achieved Voltage (V): {variables.specimen_voltage:.3f}
+    Specimen Max Achieved Pulse Voltage (V): {variables.pulse_voltage:.3f}
+    Last detection rate: {variables.detection_rate_current_plot:.3f}%
+    -----------------------------------------------------
+    """
+    software_info = "Created by PyCCAPT software."
+
+    with open(variables.path + '\\parameters.txt', 'w') as f:
+        f.write(header)
+        f.write(statistics)
+        f.write(software_info)
